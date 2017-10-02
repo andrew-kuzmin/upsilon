b4adf0fb7e45b81b345047b4f1220e6c0fbb5be9
Andriy <andriy.kuzmin@scalors.com>
2017-10-02 13:23:44 +0300

    Add UserService functionality

--- src/main/java/com/dev/upsilon/service/UserService.java
+++ src/main/java/com/dev/upsilon/service/UserService.java
@@ -3,6 +3,7 @@ package com.dev.upsilon.service;
 import com.dev.upsilon.db.DatabaseClass;
 import com.dev.upsilon.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,10 +12,27 @@ public class UserService {
     private Map<String, User> usersMap = DatabaseClass.getUsers();
 
     public List<User> getAllUsers(){
-        return null;
+        return new ArrayList<User>(usersMap.values());
     }
 
     public User getUserById(String id){
         return new User();
     }
+
+    public User addUser(User User) {
+        User.setId(String.valueOf(usersMap.size() + 1));
+        usersMap.put(User.getId(), User);
+        return User;
+    }
+
+    public User updateUser(User User) {
+        usersMap.put(User.getId(), User);
+        return User;
+    }
+
+    public User removeUser(String id) {
+        return usersMap.remove(id);
+    }
+
+
 }
